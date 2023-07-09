988da57b64a802ac20a8acbc9bef4a4cdaf5bdbd
Mauricio Bessón <mauribesson@gmail.com>
2023-07-09 03:12:27 -0300

    add: seleccion de formulario

--- cypress/e2e/00-mis-pruebas/001-hola-mundo.cy.js
+++ cypress/e2e/00-mis-pruebas/001-hola-mundo.cy.js
@@ -4,14 +4,31 @@
 
 describe('Practica de Cypress Automation 101', () => {
 
-    beforeEach(() => {
-        cy.visit('https://demoqa.com/')
-    })
+   /* beforeEach(() => {
+        //cy.visit('https://demoqa.com/')
+        cy.visit('https://demoqa.com/automation-practice-form')
+    })*/
 
-    it('TC001_Acceso', () => {
+    /*it('TC001_Acceso', () => {
         let app = cy.get('#app').should()
         console.log('Selector de APP', app)
         cy.get('header > a > img')
+    })*/
+
+    it('Accesos a formulario', () => {
+      cy.visit('https://demoqa.com/automation-practice-form')
+      cy.get('h5').should('have.text', 'Student Registration Form')
+      
+    })
+
+    it('Priemer nombre', () => {
+      cy.visit('https://demoqa.com/automation-practice-form')
+      
+      let inputFirstName = cy.get('#firstName')
+      console.log('atributo', inputFirstName)
+      /*let firstName = 'Text'
+      cy.get('#firstName').type(firstName)
+      */
     })
 })
 
